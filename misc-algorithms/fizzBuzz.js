874ca2acd630bb06e3cc78aea23c716a35122cc4
Chandra Holt <chandra_holt@hotmail.com>
2022-09-12 16:50:20 -0700

    Added simple fizzbuzz function

---
+++ misc-algorithms/fizzBuzz.js
@@ -0,0 +1,35 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  // initialize new array variable to return
+  let myNewArray = [];
+  //loop through array indices
+  for (let i = 1; i <= n; i++) {
+    //set conditionals here
+    if (i % 3 === 0 && i % 5 == 0) {
+      myNewArray.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      myNewArray.push("Fizz");
+    } else if (i % 5 == 0) {
+      myNewArray.push("Buzz");
+    } else {
+      myNewArray.push(i.toString());
+    }
+  }
+
+  return myNewArray;
+};
+
+fizzBuzz(3);
+// fizzBuzz(10)
+// fizzBuzz(23)
+// fizzBuzz(121)
